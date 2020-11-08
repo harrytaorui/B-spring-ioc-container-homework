4583755de3ad07068e20d784806346a30c319619
ruitao <harrytaorui@gmail.com>
2020-11-08 11:40:57 +0800

    [taorui]:p2s2

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,26 +1,21 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.WebApplicationContext;
+
 
 @RestController
-public class GreetingController implements ApplicationContextAware {
+public class GreetingController {
 
-    private ApplicationContext applicationContext;
+    @Autowired
+    private GreetingService greetingService;
 
     @GetMapping("/greet")
     public String greet() {
-        GreetingService greetingService = applicationContext.getBean(GreetingService.class);
+        System.out.println(greetingService.hashCode());
         return greetingService.sayHi();
     }
 
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
 }
