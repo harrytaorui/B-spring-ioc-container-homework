bf7c1ab1f1817e93381f4ccc998dcab15ebd4ed2
ruitao <harrytaorui@gmail.com>
2020-11-08 12:06:03 +0800

    [taorui]: p3s3 using postconstruct

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,7 +1,5 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
